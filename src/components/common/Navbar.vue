7db2d55e56525102f9fc0a08bd41bcf678353429
crumpx <crumpx@gmail.com>
2017-03-15 18:58:02 -0400

    first commit

---
+++ src/components/common/Navbar.vue
@@ -0,0 +1,109 @@
+<template lang="html">
+  <header>
+    <nav class='navbar '>
+      <div class="container clearfix">
+      <div class="brand">
+        <strong>Gang</strong>
+        </div>
+      <div class="nav-toggle" v-on:click="openMenu()">☰</div>
+
+      <div v-bind:class="[ isOpen ? 'open' : 'close', 'nav-menu']">
+        <ul>
+          <!-- <li v-on:click="openMenu()"><router-link to="" title='Home'>Home</router-link></li> -->
+          <li v-on:click="openMenu()"><router-link to="" title='About Me'>About</router-link></li>
+          <li v-on:click="openMenu()"><router-link to="" title='My Portfolio'>Portfolio</router-link></li>
+          <li v-on:click="openMenu()"><router-link to="" title='Services I offer'>Service</router-link></li>
+          <li v-on:click="openMenu()"><router-link to="" title='My Blog'>Blog</router-link></li>
+        </ul>
+      </div>
+      </div>
+    </nav>
+    <div class="spacer"></div>
+  </header>
+</template>
+
+<script>
+
+export default {
+  name: 'navbar',
+  data() {
+    return {
+      isOpen: false
+    }
+  },
+  methods: {
+   openMenu: function(){
+     this.isOpen = !this.isOpen
+   },
+ },
+
+}
+</script>
+
+<style lang="sass" scoped>
+  .open
+    left: 0
+    opacity: 1
+    transition: all 0.6s ease-in-out
+  .close
+    left: -100%
+    opacity: 0
+    transition: all 0.6s ease-in-out
+    @media screen and (min-width: 760px)
+      opacity: 1
+  .spacer
+    height: 61px
+  .navbar
+    font-family: 'Open Sans', sans-serif
+    z-index: 1
+    position: fixed
+    width: 100%
+    background: rgba(255,255,255, 0.8)
+    border-bottom: 1px solid black
+    .container
+      width: 100%
+      margin: 0 auto
+      .brand
+        float: left
+        margin-left: 20px
+        strong
+          font-size: 30px
+          line-height: 60px
+          font-weight: 700
+      .nav-toggle
+        float: right
+        line-height: 60px
+        margin-right: 20px
+        @media screen and (min-width: 790px)
+          display: none
+      .nav-menu
+        /*z-index: 1*/
+        width: 100%
+        float: right
+        position: absolute
+        top: 61px
+        background: rgba(255,255,255, 0.8)
+        border-bottom: 1px solid black
+        @media screen and (min-width: 790px)
+          position: static
+          margin-right: 20px
+          display: block
+          width: initial
+          border: 0
+        li
+          @media screen and (min-width: 790px)
+            display: inline-block
+          a
+            padding: 0 18px
+            line-height: 60px
+            display: block
+            text-decoration: none
+            color: black
+            font-weight: 600
+            color: #666
+            transition: all 0.3s ease-in
+            &:hover
+              background: #999
+              color: #fff
+
+</style>
