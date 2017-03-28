319f6caef9df8d077f68bfeb4facb4591629b65d
crumpx <crumpx@gmail.com>
2017-03-27 21:24:50 -0400

    editor

---
+++ src/components/Blog/Blogs.vue
@@ -0,0 +1,45 @@
+<template lang="html">
+  <section>
+    <div class="blog-list">
+      <div class="container">
+        <blog v-for="blog of blogs" v-bind:blg="blog" :key="blog.id"></blog>
+    </div> <!-- container -->
+    </div>
+
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+import Blog from './Blog'
+export default {
+  data() {
+    return {
+      blogs: {}
+    }
+  },
+  methods: {
+    fetch() {
+        axios.get('http://localhost:3000/').then(response =>{
+          this.blogs = response.data;
+        }).catch(error => console.log(error))
+    }
+  },
+  components: {
+    'blog': Blog
+  },
+  created: function(){
+    this.fetch();
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+  section
+    background: #eee
+    .blog-list
+      background: #eee
+      .container
+        max-width: 790px
+        margin: 0 auto
+</style>
---
+++ src/components/Blog/Editor.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <content>
+    
+  </content>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
